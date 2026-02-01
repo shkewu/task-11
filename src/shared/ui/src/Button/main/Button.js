ece67bf4430bc4d2775d8f92440902e2d7bf1122
Daniil <jonehami@mail.ru>
2026-02-01 21:51:14 +0300

    fix(Button): исправил проверку на добавление текста и img в кнопку

--- src/shared/ui/src/Button/main/Button.js
+++ src/shared/ui/src/Button/main/Button.js
@@ -1,20 +1,20 @@
-// это будет универсальная функция создания кнопки, которая будет принимать
+// этот массив нужно расположить в файле content непосредственно в папке элемента, для которого будем создавать кнопку
 
-const events = [
-  {
-    event: "click",
-    callback() {
-      console.log("click"); // логика кнопки, которую мы создаем
-    },
-    options: {
-      passive: true,
-      once: true
-    }
-  }
-  // ...
-];
+// const events = [
+//   {
+//     event: "click",
+//     callback() {
+//       console.log("click"); // логика кнопки, которую мы создаем
+//     },
+//     options: {
+//       passive: true,
+//       once: true
+//     }
+//   }
+//   // ...
+// ];
 
-function Button({events = [], disabled = false, timeout = 300, children = null, className = ""}) {
+function Button({events = [], disabled = false, timeout = 300, src = null, children = null, className = ""}) {
   const button = document.createElement("button");
   button.classList.add("button");
   button.className = `${className}`;
@@ -28,17 +28,20 @@ function Button({events = [], disabled = false, timeout = 300, children = null, 
     });
   });
 
-    if(typeof children ===  "string") {
-      if (children.match(/\.(jpg|jpeg|png|svg)$/i)) {
-        const child = document.createElement("img");
-        child.src = children;
-      } else {
-        const child = document.createElement("div");
-        child.textContent = children;
-      }
+  if (typeof children === "string") {
+    const child = document.createElement("div");
+    child.textContent = children;
+    button.appendChild(child);
+  }
 
+  if (!!src) {
+    const child = document.createElement("img");
+    child.src = src;
+    child.alt = "image";
+    button.appendChild(child);
+  }
 
-      if (disabled) {
+  if (disabled) {
     button.disabled = true;
   }
 
