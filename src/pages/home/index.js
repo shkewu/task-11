b22a2b0b7b63e63d6da17142860f1c4fc7b75a87
Daniil <jonehami@mail.ru>
2026-02-07 20:47:47 +0300

    feat(index): добавил вызов Players.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import {modalProvider, ModalProvider} from "../../app";
 import {Modal as PlayerModal} from "../../features/playersModal";
 
 import Shared from "@shared";
+import {Players} from "../../widgets/players/ui/Players";
 
 const {lib: {image}, api: {get}} = Shared;
 
@@ -11,14 +12,16 @@ get("users").then(user => {
   console.log(user);
 });
 
-const modalProviderDOM = ModalProvider();
-modalProvider.init(modalProviderDOM, {playerModal: PlayerModal});
-modalProvider.addModal(
-  "playerModal",
-  {
-    name: "dwqwqd",
-    description: "dwqqwd",
-    imgSrc: image("players/left.png")
-  }
-);
+Players()
+
+// const modalProviderDOM = ModalProvider();
+// modalProvider.init(modalProviderDOM, {playerModal: PlayerModal});
+// modalProvider.addModal(
+//   "playerModal",
+//   {
+//     name: "dwqwqd",
+//     description: "dwqqwd",
+//     imgSrc: image("players/left.png")
+//   }
+// );
 
